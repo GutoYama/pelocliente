edb7c5f521f961a9b3499d8cd4a916150456f42a
eduardo <edjo.terra@gmail.com>
2025-10-24 23:58:11 -0300

    FKs acho q deu certo

--- pelocliente_db.sql
+++ pelocliente_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 24/10/2025 às 01:30
+-- Tempo de geração: 25/10/2025 às 04:50
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -24,74 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `cliente`
---
-
-CREATE TABLE `cliente` (
-  `cod_cliente` int(11) NOT NULL,
-  `nome` varchar(100) NOT NULL,
-  `cpf` varchar(11) NOT NULL,
-  `endereco` varchar(200) NOT NULL,
-  `telefone` varchar(20) NOT NULL,
-  `email` varchar(70) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `composicao`
---
-
-CREATE TABLE `composicao` (
-  `cod_prato` int(11) NOT NULL,
-  `cod_ingrediente` int(11) NOT NULL,
-  `quantidade` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `compra`
---
-
-CREATE TABLE `compra` (
-  `cod_compra` int(11) NOT NULL,
-  `cod_ingrediente` int(11) NOT NULL,
-  `cod_fornecedor` int(11) NOT NULL,
-  `quantidade` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `fornecedor`
---
-
-CREATE TABLE `fornecedor` (
-  `cod_fornecedor` int(11) NOT NULL,
-  `nome_fantasia` varchar(100) NOT NULL,
-  `endereco` varchar(100) NOT NULL,
-  `cnpj` varchar(14) NOT NULL,
-  `telefone` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `ingrediente`
---
-
-CREATE TABLE `ingrediente` (
-  `cod_ingrediente` int(11) NOT NULL,
-  `descricao` varchar(100) NOT NULL,
-  `quantidade` float NOT NULL,
-  `cod_unidade` int(11) NOT NULL,
-  `valor_unit` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Estrutura para tabela `itens_pedido`
 --
 
@@ -101,135 +33,27 @@ CREATE TABLE `itens_pedido` (
   `quantidade` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `pedido`
---
-
-CREATE TABLE `pedido` (
-  `cod_pedido` int(11) NOT NULL,
-  `cod_cliente` int(11) NOT NULL,
-  `valor_total` float NOT NULL,
-  `endereco` varchar(100) NOT NULL,
-  `datahora` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `prato`
---
-
-CREATE TABLE `prato` (
-  `cod_prato` int(11) NOT NULL,
-  `descricao` varchar(100) NOT NULL,
-  `valor` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `unidade`
---
-
-CREATE TABLE `unidade` (
-  `cod_unidade` int(11) NOT NULL,
-  `descricao` varchar(50) NOT NULL,
-  `sigla` varchar(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices de tabela `cliente`
+-- Índices de tabela `itens_pedido`
 --
-ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`cod_cliente`);
+ALTER TABLE `itens_pedido`
+  ADD KEY `fk_itens_pedido_pedido` (`cod_pedido`),
+  ADD KEY `fk_itens_pedido_prato` (`cod_prato`);
 
 --
--- Índices de tabela `compra`
---
-ALTER TABLE `compra`
-  ADD PRIMARY KEY (`cod_compra`);
-
---
--- Índices de tabela `fornecedor`
---
-ALTER TABLE `fornecedor`
-  ADD PRIMARY KEY (`cod_fornecedor`);
-
---
--- Índices de tabela `ingrediente`
---
-ALTER TABLE `ingrediente`
-  ADD PRIMARY KEY (`cod_ingrediente`);
-
---
--- Índices de tabela `pedido`
---
-ALTER TABLE `pedido`
-  ADD PRIMARY KEY (`cod_pedido`);
-
---
--- Índices de tabela `prato`
---
-ALTER TABLE `prato`
-  ADD PRIMARY KEY (`cod_prato`);
-
---
--- Índices de tabela `unidade`
---
-ALTER TABLE `unidade`
-  ADD PRIMARY KEY (`cod_unidade`);
-
---
--- AUTO_INCREMENT para tabelas despejadas
+-- Restrições para tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT de tabela `cliente`
+-- Restrições para tabelas `itens_pedido`
 --
-ALTER TABLE `cliente`
-  MODIFY `cod_cliente` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `compra`
---
-ALTER TABLE `compra`
-  MODIFY `cod_compra` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `fornecedor`
---
-ALTER TABLE `fornecedor`
-  MODIFY `cod_fornecedor` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `ingrediente`
---
-ALTER TABLE `ingrediente`
-  MODIFY `cod_ingrediente` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `pedido`
---
-ALTER TABLE `pedido`
-  MODIFY `cod_pedido` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `prato`
---
-ALTER TABLE `prato`
-  MODIFY `cod_prato` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `unidade`
---
-ALTER TABLE `unidade`
-  MODIFY `cod_unidade` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `itens_pedido`
+  ADD CONSTRAINT `fk_itens_pedido_pedido` FOREIGN KEY (`cod_pedido`) REFERENCES `pedido` (`cod_pedido`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_itens_pedido_prato` FOREIGN KEY (`cod_prato`) REFERENCES `prato` (`cod_prato`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
