e0a643e5cee2fab11dfcde88fa78742e416c7f55
eduardo <edjo.terra@gmail.com>
2025-10-30 21:54:02 -0300

    Quase pronto ein

--- resources/views/editarpedido.blade.php
+++ resources/views/editarpedido.blade.php
@@ -1,62 +1,15 @@
 <!DOCTYPE html>
 <html>
-    <body>
-        @include('partials.nav', ['x'=>0])
-        @include('partials.formularios')
-        <form action='adicionar_bd' method='POST'>    
-        @csrf
-            <div id='formularioClienteCadastrado'>
-                <label>Cliente</label>
-                <select id='opcoesDeClientes' name='cliente'>
-                    <option></option>
-                    
-                    @foreach($clientes as $cliente)
-                        <option value='{{$cliente->cod_cliente}}'>{{$cliente->nome}}</option>
-                    @endforeach
-                </select>
+    <head>
+        <script>
+            var contadorDeItens = 0;
+            var clientes = @json($clientes);
+            var pratos = @json($pratos);
+            var itens_pedido = @json($itens_pedido);
 
-                <div id='dadosDoCliente'>
 
-                    <div class="endereco">
-                        <label>Endereço: </label>
-                        <div id='endereco'></div>
-                    </div>
-                    
-                    <div class="cpf">
-                        <label>CPF: </label>
-                        <div id='cpf'></div>    
-                    </div>
-                
-                    <label>Endereço</lebal>
-                    <input type='text' value='{{$pedido[0]->endereco}}'>
-
-                </div>
-            </div>
-
-            <button class="botaoPedido" type='button' onclick='adicionarItem()'>Adicionar Item</button>
-
-            @foreach($itens_pedido as $prato)
-
-            @endforeach
-            
-            <div id='itensDoPedido'>
-            </div>
-
-            <p id='valorTotal'>Valor Total: R$ 0.0</p>
-
-            <center><input class="enviarPedido" type='submit' value='Finalizar Pedido'></center>
-        </form>
-    </body>
-
-    <script>
-        var opcoesDeCliente = document.getElementById('opcoesDeClientes');
+            var opcoesDeCliente = document.getElementById('opcoesDeClientes');
         opcoesDeCliente.addEventListener('change', alterarDadosCliente);
-
-        var clientes = @json($clientes);
-        var pratos = @json($pratos);
-        var itens_pedido = @json($itens_pedido);
-
-        var contadorDeItens = 0;
 
         function adicionarItem()
         {
@@ -105,12 +58,42 @@
             divItensDoPedido.appendChild('<br>');
         }
 
-        function adicionarItemAdicionado(item_pedido)
+        function calcularValorTotal()
+        {
+            var itensPedidoDiv = document.getElementById('itensDoPedido');
+            var itensPedido = itensPedidoDiv.children;
+
+            var valorTotal = 0;
+
+            for (var i = 0; i < itensPedido.length; i++)
+            {
+                if (itensPedido[i].id == 'prato')
+                {
+                    var valor_unitario = itensPedido[i].selectedOptions[0].dataset.valor_unitario;
+                    
+
+                    for (var j = i; j < itensPedido.length; j++)
+                    {
+                        if (itensPedido[j].id == 'quantidade')
+                        {
+                            valorTotal += valor_unitario * itensPedido[j].value;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            document.getElementById('valorTotal').innerText = 'Valor Total: R$ ' + valorTotal.toString();
+        }
+
+
+
+            function adicionarItemAdicionado(item_pedido)
         {
             contadorDeItens++;
 
             var selectPrato = document.createElement('select');
-            selectPrato.addEventListener('change', calcularValorTotal);
+            //selectPrato.addEventListener('change', calcularValorTotal);
             selectPrato.name = 'pratos[]';
             selectPrato.id = 'prato';
 
@@ -121,7 +104,8 @@
             inputQuantidade.name = 'quantidades[]';
             inputQuantidade.type = 'number';
             inputQuantidade.id = 'quantidade';
-            inputQuantidade.addEventListener('input', calcularValorTotal);
+            inputQuantidade.value = itens_pedido[item_pedido];
+            //inputQuantidade.addEventListener('input', calcularValorTotal);
 
             var labelQuantidade = document.createElement('label');
             labelQuantidade.innerText = "Quantidade ";
@@ -138,6 +122,14 @@
                 optionPrato.value = pratos[i].cod_prato;
                 optionPrato.innerText = pratos[i].descricao;
                 
+                for (var j = 0; j < itens_pedido.length; j++)
+                {
+                    if (itens_pedido[j]['cod_prato'] == pratos[i])
+                    {
+                        optionPrato.selected = true;
+                    }
+                }
+
                 selectPrato.appendChild(optionPrato);
             }
 
@@ -168,33 +160,69 @@
                 }
             }
         }
+    
+        </script>
+    </head>
 
-        function calcularValorTotal()
-        {
-            var itensPedidoDiv = document.getElementById('itensDoPedido');
-            var itensPedido = itensPedidoDiv.children;
-
-            var valorTotal = 0;
-
-            for (var i = 0; i < itensPedido.length; i++)
-            {
-                if (itensPedido[i].id == 'prato')
-                {
-                    var valor_unitario = itensPedido[i].selectedOptions[0].dataset.valor_unitario;
+    <body>
+        @include('partials.nav', ['x'=>0])
+        @include('partials.formularios')
+        <form action='adicionar_bd' method='POST'>    
+        @csrf
+            <div id='formularioClienteCadastrado'>
+                <label>Cliente</label>
+                <select id='opcoesDeClientes' name='cliente'>
+                    <option></option>
                     
+                    @foreach($clientes as $cliente)
+                        <option value='{{$cliente->cod_cliente}}'>{{$cliente->nome}}</option>
+                    @endforeach
+                </select>
 
-                    for (var j = i; j < itensPedido.length; j++)
-                    {
-                        if (itensPedido[j].id == 'quantidade')
-                        {
-                            valorTotal += valor_unitario * itensPedido[j].value;
-                            break;
-                        }
-                    }
-                }
-            }
+                <div id='dadosDoCliente'>
 
-            document.getElementById('valorTotal').innerText = 'Valor Total: R$ ' + valorTotal.toString();
-        }
+                    <div class="endereco">
+                        <label>Endereço: </label>
+                        <div id='endereco'></div>
+                    </div>
+                    
+                    <div class="cpf">
+                        <label>CPF: </label>
+                        <div id='cpf'></div>    
+                    </div>
+                
+                    <label>Endereço</lebal>
+                    <input type='text' value='{{$pedido[0]->endereco}}'>
+
+                </div>
+            </div>
+
+            <button class="botaoPedido" type='button' onclick='adicionarItem()'>Adicionar Item</button>
+
+            <div id="itensDoPedido">
+            @foreach($itens_pedido as $item_pedido)
+                <label>Prato</label>
+                <select name="pratos[]" id="prato">
+                    <option value=""></option>
+
+                    @foreach($pratos as $prato)
+                        <option data-valor_unitario="{{$prato->valor}}" value="{{$prato->cod_prato}}" {{$item_pedido->cod_prato == $prato->cod_prato ? 'selected' : ''}} >{{$prato->descricao}}</option>
+                    @endforeach
+                </select>
+
+                <label>Quantidade</label>
+                <input name="quantidades[]" type="number" id="quantidade" value='{{$item_pedido->quantidade}}'>
+
+            @endforeach
+            </div>
+
+            <p id='valorTotal'>Valor Total: R$ 0.0</p>
+
+            <center><input class="enviarPedido" type='submit' value='Finalizar Pedido'></center>
+        </form>
+    </body>
+
+    <script>
+        
     </script>
 </html>
