2681049e96a29f31a77d96be9389976c577a94cf
eduardo <edjo.terra@gmail.com>
2025-11-01 01:44:37 -0300

    Editar pedido funcionando

--- resources/views/editarpedido.blade.php
+++ resources/views/editarpedido.blade.php
@@ -39,13 +39,16 @@
             <button class="botaoPedido" type='button' onclick='adicionarItem()'>Adicionar Item</button>
 
             <div id="itensDoPedido">
+            
+            @php $contador = 0; @endphp
             @foreach($itens_pedido as $item_pedido)
+                @php $contador += 1; @endphp
                 <script>contadorDeItens++;</script>
 
                 <div>
 
                     <label>Prato</label>
-                    <select name="pratos[]" id="prato">
+                    <select name="pratos[]" id="prato{{$contador}}" onchange='verificarPrato(event);reacalcularValorTotal()'>
                         <option value=""></option>
 
                         @foreach($pratos as $prato)
@@ -54,7 +57,7 @@
                     </select>
 
                     <label>Quantidade</label>
-                    <input name="quantidades[]" type="number" id="quantidade" value='{{$item_pedido->quantidade}}'>
+                    <input name="quantidades[]" type="number" id="quantidade" value='{{$item_pedido->quantidade}}' oninput='reacalcularValorTotal()'>
 
                     <button type='button' onclick='excluirIngrediente(this)'>Lixeira/Excluir ingrediente</button>
 
@@ -77,11 +80,49 @@
 
         var contadorDeItens = 0;
 
+        var selects_pratos_originais = document.getElementById('itensDoPedido').querySelectorAll('select');
+
+        var pratos_selecionados_originais = {};
+
+        for (var i = 0; i < selects_pratos_originais.length; i++)
+        {
+            var select = selects_pratos_originais[i];
+            pratos_selecionados_originais[select.id] = select.selectedIndex;
+        }
+
+        console.log(pratos_selecionados_originais);
+
+
+        function verificarPrato(event)
+        {
+            var selects_pratos = document.getElementById('itensDoPedido').querySelectorAll('select');
+            var cod_prato_selecionado = event.target.selectedOptions[0].value;
+
+            for (var i = 0; i < selects_pratos.length; i++)
+            {
+                if (event.target != selects_pratos[i])
+                {
+                    if (cod_prato_selecionado == selects_pratos[i].selectedOptions[0].value)
+                    {
+                        alert('Prezado(a) funcionário, não é permitido a inclusão do mesmo prato mais de uma vez.');
+
+                        if (event.target.id == 'pratoNovo' )
+                            event.target.selectedIndex = 0;
+                        else
+                            event.target.selectedIndex = pratos_selecionados_originais[event.target.id];
+                        break;
+                    }
+                }
+            }
+        }
+
         function excluirIngrediente(botaoExcluirIgrediente)
         {
             var divIngrediente = botaoExcluirIgrediente.parentElement;
 
             divIngrediente.remove();
+
+            reacalcularValorTotal();
         }
 
         function modificarParaCliente(isCadastrado)
@@ -106,9 +147,10 @@
             contadorDeItens++;
 
             var selectPrato = document.createElement('select');
-            selectPrato.addEventListener('change', calcularValorTotal);
+            selectPrato.addEventListener('change', reacalcularValorTotal);
+            selectPrato.addEventListener('change', verificarPrato);
             selectPrato.name = 'pratos[]';
-            selectPrato.id = 'prato';
+            selectPrato.id = 'pratoNovo';
 
             var labelPrato = document.createElement('label');
             labelPrato.innerText = "Prato ";
@@ -117,7 +159,7 @@
             inputQuantidade.name = 'quantidades[]';
             inputQuantidade.type = 'number';
             inputQuantidade.id = 'quantidade';
-            inputQuantidade.addEventListener('input', calcularValorTotal);
+            inputQuantidade.addEventListener('input', reacalcularValorTotal);
 
             var labelQuantidade = document.createElement('label');
             labelQuantidade.innerText = "Quantidade ";
@@ -179,32 +221,24 @@
             }
         }
 
-        function calcularValorTotal()
+        function reacalcularValorTotal()
         {
-            var itensPedidoDiv = document.getElementById('itensDoPedido');
-            var itensPedido = itensPedidoDiv.children;
+            var selects_pratos = document.getElementById('itensDoPedido').querySelectorAll('select');
+            var quantidades_pratos = document.getElementById('itensDoPedido').querySelectorAll('input');
+            var valor_total = 0;
 
-            var valorTotal = 0;
-
-            for (var i = 0; i < itensPedido.length; i++)
+            for (var i = 0; i < selects_pratos.length; i++)
             {
-                if (itensPedido[i].id == 'prato')
+                for (var j = 0; j < pratos.length; j++)
                 {
-                    var valor_unitario = itensPedido[i].selectedOptions[0].dataset.valor_unitario;
-                    
-
-                    for (var j = i; j < itensPedido.length; j++)
+                    if (selects_pratos[i].selectedOptions[0].value == pratos[j].cod_prato)
                     {
-                        if (itensPedido[j].id == 'quantidade')
-                        {
-                            valorTotal += valor_unitario * itensPedido[j].value;
-                            break;
-                        }
+                        valor_total += pratos[j].valor * quantidades_pratos[i].value;
+
+                        document.getElementById('valorTotal').innerText = 'Valor Total: R$ ' + valor_total;
                     }
                 }
             }
-
-            document.getElementById('valorTotal').innerText = 'Valor Total: R$ ' + valorTotal.toString();
         }
     </script>
 </html>
